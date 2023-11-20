9df5e86a4b686ba17d7deaddffff209eacd6a964
Alex <txrushclash@gmail.com>
2023-11-20 19:11:52 +0200

    Added a User table to DB, also created a login and register rout, also added a function to delete task

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import secrets
 
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from flask_migrate import Migrate
 
-from tasks import Task, TaskForm, db
+from database import Task, TaskForm, User, db
 
 
 # ======================================================================================================================
@@ -13,15 +16,17 @@ def create_app():
     app.config['SECRET_KEY'] = secrets.token_hex(16)
 
     # there goes your credentials for AWS RDS
-    admin = ""
-    password = ""
-    host = ""
-    port = ""
-    database = ""
+    admin = "admin"
+    password = "adminadmin"
+    host = "taskmanager.cywfs8sp5cgm.eu-north-1.rds.amazonaws.com"
+    port = "3306"
+    database = "taskmanagerdb"
     app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqlconnector://{admin}:{password}@{host}:{port}/{database}'
     db.init_app(app)
     with app.app_context():
         db.create_all()
+
+    migrate = Migrate(app, db)
 
     return app
 
@@ -32,25 +37,78 @@ app = create_app()
 # ======================================================================================================================
 
 
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+# ======================================================================================================================
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = bcrypt.generate_password_hash(request.form['password']).decode('utf-8')
+
+        new_user = User(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('login'))
+
+    return render_template('register.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+
+        if user and bcrypt.check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('task_list'))
+        else:
+            flash('Login failed. Please check your username and password.', 'danger')
+
+    return render_template('login.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
 @app.route('/tasks')
+@login_required
 def task_list():
     tasks = Task.query.all()
     return render_template('task_list.html', tasks=tasks)
 
 
 @app.route('/tasks/add', methods=['GET', 'POST'])
+@login_required
 def add_task():
     form = TaskForm()
     if form.validate_on_submit():
         title = form.title.data
         description = form.description.data
 
-        new_task = Task(title=title, description=description)
+        new_task = Task(title=title, description=description, user=current_user)
         db.session.add(new_task)
         db.session.commit()
 
@@ -59,6 +117,7 @@ def add_task():
 
 
 @app.route('/tasks/edit/<int:task_id>', methods=['GET', 'POST'])
+@login_required
 def edit_task(task_id):
     task = Task.query.get(task_id)
     if task:
@@ -69,6 +128,18 @@ def edit_task(task_id):
             db.session.commit()
             return redirect(url_for('task_list'))
         return render_template('edit_task.html', task=task, form=form)
+    else:
+        return render_template('task_not_found.html')
+
+
+@app.route('/tasks/delete/<int:task_id>')
+@login_required
+def delete_task(task_id):
+    task = Task.query.get(task_id)
+    if task and task.user == current_user:
+        db.session.delete(task)
+        db.session.commit()
+        return redirect(url_for('task_list'))
     else:
         return render_template('task_not_found.html')
 
--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
 
 # ======================================================================================================================
@@ -12,14 +13,26 @@ db = SQLAlchemy()
 # ======================================================================================================================
 
 
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    tasks = db.relationship('Task', backref='user', lazy=True)
+
+
+# ======================================================================================================================
+
+
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
-    def __init__(self, title, description):
+    def __init__(self, title, description, user):
         self.title = title
         self.description = description
+        self.user = user
 
 
 class TaskForm(FlaskForm):
